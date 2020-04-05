1a292f8a2b8fb8846c35acacad7c3278d9d362e9
Cosmin Tudor <cosmin2dor@gmail.com>
2020-04-05 19:27:16 +0300

    Added electron-boilerplate

---
+++ client-windows/main.js
@@ -0,0 +1,41 @@
+const electron = require('electron')
+const { autoUpdater } = require('electron-updater');
+const { app, BrowserWindow, ipcMain } = electron
+
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadFile('index.html')
+    // Handle the autoupdater
+    win.once('ready-to-show', () => {
+        autoUpdater.checkForUpdatesAndNotify()
+    })
+}
+
+app.whenReady()
+    .then(createWindow)
+
+ipcMain.on('app_version', (event) => {
+    event.sender.send('app_version', { version: app.getVersion() })
+})
+
+// Auto updater notifications
+autoUpdater.on('update-available', () => {
+    win.webContents.send('update-available')
+})
+
+autoUpdater.on('update-downloaded', () => {
+    win.webContents.send('update-downloaded')
+})
+
+ipcMain.on('restart_app', () => {
+    autoUpdater.quitAndInstall();
+});
