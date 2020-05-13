bd2e52dd19797e10927282a45c5b83a5b5dc3911
BrateS <bratescu.stefan@gmail.com>
2020-05-13 22:29:32 +0300

    Adds login system.

---
+++ web/manage_account.php
@@ -0,0 +1,8 @@
+<div class="row">
+    <div class="col-lg-8 offset-lg-2 text-center mb-40">
+        <h3>
+            <a href="<?php echo $_SESSION['portal_url']; ?>">Manage your account here</a>
+            .
+        </h3>
+    </div>
+</div>
