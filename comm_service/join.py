480a99961431fe1a9e017c7eb3ba83ed7fe24c63
Cosmin Tudor <cosmin2dor@gmail.com>
2020-07-12 00:57:29 +0300

    Added join code

---
+++ comm_service/join.py
@@ -0,0 +1,42 @@
+import os
+import json
+import requests
+import logging
+
+METADATA_URL = "http://169.254.169.254/latest/user-data"
+WG_CONF_DIR = '/etc/wireguard'
+
+class Join:
+    def __init__(self):
+        self.user_data = self.get_user_data()
+
+    def get_user_data(self):
+        try:
+            user_data = requests.get(METADATA_URL, timeout=10)
+
+            return json.loads(user_data)
+        except requests.exceptions.RequestException as e:
+            logging.error("Requesting metadata failed")
+            raise SystemExit(e)
+
+        except ValueError:
+            logging.error("Failed to decode JSON from metadata")
+            raise SystemExit(-1)
+
+    def generate_config(self):
+        config = "" + \
+        "[Interface]\n" + \
+        "Address = {}\n".format(self.user_data['address']) + \
+        "PrivateKey = {}\n".format(self.user_data['private_key']) + \
+        "\n" + \
+        "[Peer]\n" + \
+        "PublicKey = {}\n".format(self.user_data['agent']['public_key']) + \
+        "Endpoint = {}\n".format(self.user_data['agent']['endpoint']) + \
+        "AllowedIPs = {}\n".format(self.user_data['agent']['allowed_ips']) + \
+        "PersistentKeepalive = {}\n".format(self.user_data['agent']['keep_alive'])
+
+        with open(WG_CONF_DIR + "/comm.conf", "w") as file:
+            file.write(config)
+
+if __name__ == "__main__":
+    join = Join()
